f6e51391f18c841f680be541dc7ea3b6fb8e72df
chriss1525 <okothchris15@gmail.com>
2023-05-30 22:16:05 +0300

    insert node as left child

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - inserts node as left child
+ * @parent: parent of node
+ * @value: value of node
+ * Return: pointer to new node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = binary_tree_node(parent, value);
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+
+	if (parent->left != NULL)
+	{
+		node->left = parent->left;
+		parent->left = node;
+		node->left->parent = node;
+	}
+	else
+	{
+		parent->left = node;
+	}
+
+	return (node);
+}
