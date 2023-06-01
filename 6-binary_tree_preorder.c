9728c600a64bee739955f38871b357a4e20b10f5
chriss1525 <okothchris15@gmail.com>
2023-06-01 18:02:22 +0300

    travers tree using preorder

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preoder - traverses the binary tree
+ * @tree: binary tree
+ * @func: function to be performed on tree
+ * Return: 1 (if node is a root) 0 (if not)
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+	{
+		return;
+	}
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
